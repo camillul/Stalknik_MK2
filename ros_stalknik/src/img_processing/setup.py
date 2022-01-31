db71c2a9711cb75d4784c4507fd68661a28fb290
camillul <camille.ulrb@gmail.com>
2022-01-31 21:27:25 +0100

    Begin ROS architecture and ppt

---
+++ ros_stalknik/src/img_processing/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'img_processing'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rikic',
+    maintainer_email='camille.ulrb@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
