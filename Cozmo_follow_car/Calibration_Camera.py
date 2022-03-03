68c0bb35f928024e2f6ab0e3e658f3cb4662275b
cgaspard3333 <mamanflorence@hotmail.fr>
2022-03-03 18:38:22 +0100

    Ajout Calibration images + Changement de base robot-camera

--- Cozmo_follow_car/Calibration_Camera.py
+++ Cozmo_follow_car/Calibration_Camera.py
@@ -36,7 +36,7 @@ prev_img_shape = None
 # in a given directory. Since no path is
 # specified, it will take current directory
 # jpg files alone
-images = glob.glob('*.jpg')
+images = glob.glob('./images_calibration/*.jpg')
  
 for filename in images:
     image = cv2.imread(filename)
---
+++ Cozmo_follow_car/Changement_base_Cozmo.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def translation(vector):
+    return np.array([[1, 0, 0, vector[0]],
+                     [0, 1, 0, vector[1]],
+                     [0, 0, 1, vector[2]],
+                     [0, 0, 0, 1]])
+
+def Rx(alpha):
+    return np.array([[1, 0, 0, 0],
+                     [0, np.cos(alpha), -np.sin(alpha), 0],
+                     [0, np.sin(alpha), np.cos(alpha), 0],
+                     [0, 0, 0, 1]])
+
+def Ry(alpha):
+    return np.array([[np.cos(alpha), 0, np.sin(alpha), 0],
+                     [0, 1, 0, 0],
+                     [-np.sin(alpha), 0, np.cos(alpha), 0],
+                     [0, 0, 0, 1]])
+        
+def Rz(alpha):
+    return np.array([[np.cos(alpha), -np.sin(alpha), 0, 0],
+                     [np.sin(alpha), np.cos(alpha), 0, 0],
+                     [0, 0, 1, 0],
+                     [0, 0, 0, 1]])
+
+L1 = 8
+L2 = 16.3
+L3 = 49
+L4 = 9.2
+alpha = 0
+
+T_rc = translation([L4,0,L3]) @ Ry(alpha) @ translation ([L2,0,-L1])
+
+print (T_rc)
